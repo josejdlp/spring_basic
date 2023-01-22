5b94ed1deb59f46777edb33a9620e93eadcb3189
josejdlp <josejdlp@gmail.com>
2023-01-22 19:31:27 +0100

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        //create the object
+        Coach coach=new TrackCoach();
+        //use the object
+        System.out.println(coach.getDailyWorkout());
+
+
+    }
+}
---
+++ src/main/java/org/example/TrackCoach.java
@@ -0,0 +1,10 @@
+package org.example;
+
+
+public class TrackCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return "Run a har 5k";
+    }
+
+}
